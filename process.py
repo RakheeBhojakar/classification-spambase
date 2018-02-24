1323ba28022d3fdcda543d675c272b30085f13a1
Nandadeep Davuluru <aditya.nandadeep@gmail.com>
2018-02-24 13:46:33 -0800

    initial, stats are up

---
+++ process.py
@@ -0,0 +1,59 @@
+import numpy, random, pandas as pd
+from sklearn.cross_validation import train_test_split
+from addict import Dict
+
+# roughly 60/40 spam not spam split will have the following stats
+# spam = 1813, 725
+# not spam = 2788, 1672
+
+class spambase(object):
+
+	def __init__(self):
+
+		self.data = pd.read_csv('spambase.data', header=None)
+		self.data.rename(columns = {57 : 'is_spam'}, inplace=True)
+		self.statistics = dict()
+
+	def generateData(self):
+
+		# separate dataset into spam and not spam 
+		spam = self.data[self.data['is_spam'] == 1]
+		notSpam = self.data[self.data['is_spam'] == 0]
+		# split using sklearn test train split - random 
+		spam_train, spam_test = train_test_split(spam, train_size=0.6)
+		notSpam_train, notSpam_test = train_test_split(notSpam, train_size=0.6)
+		# divide the data from labels for easier fitting
+		trainData = notSpam_train.append(spam_train)
+		trainLabels = trainData.pop('is_spam')
+		testData = notSpam_test.append(spam_test)
+		testLabels = testData.pop('is_spam')
+		# return them
+		return trainData, trainLabels, testData, testLabels
+
+
+	# create a df with 57 rows, and 2 columns where the 2 columns will have [mean, std]
+	# data structure is as follows:
+
+	# 0 : [m0, std0]
+	# 1 : [m1, std1]
+	# ...
+
+	def generateStatistics(self, dataframe):
+		
+		for column in train.columns:
+			strColumn = str(column)
+			meanOfCol = train[column].mean()
+			stdOfCol = train[column].std()
+			self.statistics[strColumn] = [meanOfCol, stdOfCol]
+		return self.statistics
+
+
+classifier = spambase()
+train, trainLabels, test, testLabels = classifier.generateData()
+stats = classifier.generateStatistics(train)
+
+print(stats)
+
+
+
+
